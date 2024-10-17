9d84f09f26874d2a02473854c87cd4c47a5f9fd7
Sara Valentina García Granados <sv_garciag@javeriana.edu.co>
2024-10-17 10:17:04 -0500

    Update Cliente.c

--- PipeNameBidireccional/Cliente.c
+++ PipeNameBidireccional/Cliente.c
@@ -1,14 +1,16 @@
 /*********************************************************************
-Autor: Sara Valentina GarcÃ­a
+Autor: Sara Valentina Garcí­a
 Materia: Sistemas Operativos
 Fecha: 17/10/24
 Tema: Pipe Named o FIFO o TuberÃ­a
-TÃ³pico: ComunicaciÃ³n Bidireccional 
+Tópico: Comunicación Bidireccional 
 
 *********************************CLIENTE******************************
 Pipe named: TuberÃ­a con nombre que permite la comuncaciÃ³n de manera
 bidireccional
 
+El cliente manda un mensaje al servidor por medio de la conección a una tubería
+bidireccional (/tamp/fifo/twowat)
 *********************************************************************/
 #include <stdio.h>
 #include <sys/stat.h>
