4537098a704942d28a980ea48cce4b7f8f0ef153
Sara Valentina García Granados <sv_garciag@javeriana.edu.co>
2024-10-19 15:42:08 -0500

    Update Calloc.c

--- TallerMalloc/Calloc.c
+++ TallerMalloc/Calloc.c
@@ -1,35 +1,43 @@
-/********************************************************************************************
-autor: jorge Sierra
-materia; Sistemas Operativos
-fecha: 8/08/2024
-tema: uso de calloc en un codigo de c.
- ********************************************************************************************/
+/**********************************************************
+Estudiante en formación: Sara Valentina García 
+Materia: Sistemas Operativos
+Fecha: 8/08/24
+Tópico: Uso de calloc en codigo de c
+ *********************************************************/
 
-
-  
-#include <stdio.h>
+//Librerías 
+#include <stdio.h> 
 #include <stdlib.h>
 
 int main(int argc, char *argv){
 
-  // presenta 3 formas diferentes de reservar espacio de memoria de 4 enteros
-  int *varptr1=calloc(4*sizeof(int));
-  int *varptr2=calloc(sizeof(int[4]));
-  int *varptr3=calloc(4*sizeof*varptr3);
+  // Se presentaan 3 formas diferentes para reservar espacio en memoria de 4 números enteros
+  int *varptr1 = calloc(4, sizeof(int));
+  int *varptr2 = calloc(4, sizeof(int));
+  int *varptr3 = calloc(4, sizeof *varptr3);
 
-  
+  //Se verifica si varptr1 fue asignado correctamente
   if(varptr1){
-    //se rellana el espacio en memoria y se imprime con lo que se lleno
-    for(int i=0;i<4;i++)
+    //se rellana el espacio en memoria 
+    for(int i=0;i<4;i++){
+      //Se imprimen los valores asignados 
       varptr1[i]=i*i;
-    for(int i=0;i<4;i++)
+    }
+    
+    for(int i=0;i<4;i++){
+      //Se imprime el contenido de varptr1 en cada posición
       printf("varptr1[%d]\t == %d\n",i,varptr1[i]);
+    }
   }
-  // se libera la memoria
+  
+  // se indica que se va a liberar memoria
   printf("Se libera memoria\n");
 
-
-  free(varptr1);
-  free(varptr2);
-  free(varptr3); 
+  //Si no esta vacío se libera memoria para cada puntero 
+  if (varptr1) free(varptr1);  
+  if (varptr2) free(varptr2);    
+  if (varptr3) free(varptr3);
+  
+  //Fin del programa 
   return 0;
+}
