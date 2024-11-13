4cef1246e1973a3171f04c2ac20bbf66138adc70
Sara Valentina García Granados <sv_garciag@javeriana.edu.co>
2024-11-12 20:51:09 -0500

    Update Publicador.h

--- Proyecto/Publicador.h
+++ Proyecto/Publicador.h
@@ -1,1 +1,9 @@
+#ifndef PUBLICADOR_H
+#define PUBLICADOR_H
+
+bool validarFormatoNoticia(const char* noticia);
+void publicarNoticia(int pipe, const char* noticia);
+void ejecutarPublicador(const char* pipePSC, const char* archivoNoticias, int tiempo);
+
+#endif // PUBLICADOR_H
 
