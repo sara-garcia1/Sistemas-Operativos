d0abd07e69625d50843d3bcad29ebb8a1a0eb959
Sara Valentina García Granados <sv_garciag@javeriana.edu.co>
2024-11-12 20:51:58 -0500

    Update main.c

--- Proyecto/main.c
+++ Proyecto/main.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "publicador.h"
+
+int main(int argc, char* argv[]) {
+    const char* pipePSC = NULL;
+    const char* archivoNoticias = NULL;
+    int tiempo = 0;
+
+    // Procesar los argumentos de la línea de comandos
+    int opt;
+    while ((opt = getopt(argc, argv, "p:f:t:")) != -1) {
+        switch (opt) {
+            case 'p':
+                pipePSC = optarg;
+                break;
+            case 'f':
+                archivoNoticias = optarg;
+                break;
+            case 't':
+                tiempo = atoi(optarg);
+                break;
+            default:
+                fprintf(stderr, "Uso incorrecto de los argumentos.\n");
+                return 1;
+        }
+    }
+
+    // Verificar que los argumentos sean válidos
+    if (pipePSC == NULL || archivoNoticias == NULL || tiempo <= 0) {
+        fprintf(stderr, "Faltan argumentos necesarios.\n");
+        return 1;
+    }
+
+    // Ejecutar el publicador con los argumentos proporcionados
+    ejecutarPublicador(pipePSC, archivoNoticias, tiempo);
+
+    return 0;
+}
 
