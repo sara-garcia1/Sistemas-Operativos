58bb7f5c9f34ce73627009f61ef861d4b4f9c641
Sara Valentina García Granados <sv_garciag@javeriana.edu.co>
2024-11-12 21:36:03 -0500

    Update Publicador.h

--- Proyecto/Publicador.h
+++ Proyecto/Publicador.h
@@ -1,9 +1,19 @@
-#ifndef PUBLICADOR_H
+/********************************************************************************
+MEMBRETE
+Integrantes del grupo: Sara García, Alejandro Caicedo, Sergio, Nicolás
+Materia: Sistemas Operativos
+Tópico: Proyecto
+Descripción del archivo: Declarar las funciones que se usarán en "publicador.c"
+para mantener el orden de todas las operaciones que se realizarán allí.
+********************************************************************************/
+
+#ifndef PUBLICADOR_H 
 #define PUBLICADOR_H
+
+#include <stdbool.h>
 
 bool validarFormatoNoticia(const char* noticia);
 void publicarNoticia(int pipe, const char* noticia);
 void ejecutarPublicador(const char* pipePSC, const char* archivoNoticias, int tiempo);
 
-#endif // PUBLICADOR_H
-
+#endif // PUBLICADOR_H 
