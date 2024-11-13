a1552e7aec860f39a1c19be61f0865cfa86333ba
Sara Valentina García Granados <sv_garciag@javeriana.edu.co>
2024-11-12 22:23:51 -0500

    Update main.c

--- Proyecto/main.c
+++ Proyecto/main.c
@@ -1,3 +1,13 @@
+/********************************************************************************************
+MEMBRETE
+Integrantes del grupo: Sara García, Alejandro Caicedo, Sergio, Nicolás
+Materia: Sistemas Operativos
+Tópico: Proyecto
+Descripción del archivo: En este archivo se procesan los argumentos que el usuario introduce 
+al ejecutar el programa.
+*********************************************************************************************/
+
+//Librerias necesarias
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,20 +15,27 @@
 #include "publicador.h"
 
 int main(int argc, char* argv[]) {
+    //Variable que almacena la ruta de la tubería
     const char* pipePSC = NULL;
+    //Variable que almacena la ruta del archivo
     const char* archivoNoticias = NULL;
+    //Variable para almacenar el tiempo de publicación entre cada noticia
     int tiempo = 0;
 
-    // Procesar los argumentos de la línea de comandos
+    // Se procesan los argumentos de la línea de comandos
     int opt;
     while ((opt = getopt(argc, argv, "p:f:t:")) != -1) {
         switch (opt) {
+            //optarg equivale al valor asociado con cada opción
+            //Ruta del pipe
             case 'p':
                 pipePSC = optarg;
                 break;
+            //Ruta del archivo
             case 'f':
                 archivoNoticias = optarg;
                 break;
+            //Tiempo de espera entre noticias
             case 't':
                 tiempo = atoi(optarg);
                 break;
@@ -28,15 +45,15 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    // Verificar que los argumentos sean válidos
+    //Si los argumentos son inválidos se muestra mensaje de error
     if (pipePSC == NULL || archivoNoticias == NULL || tiempo <= 0) {
         fprintf(stderr, "Faltan argumentos necesarios.\n");
         return 1;
     }
 
-    // Ejecutar el publicador con los argumentos proporcionados
+    //Se ejecutar el publicador con los argumentos proporcionados
     ejecutarPublicador(pipePSC, archivoNoticias, tiempo);
 
     return 0;
-}
+} 
 
